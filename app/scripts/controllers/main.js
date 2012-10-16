db86ea74a039716daa9acf813b6420f5e7b1ee6e
steflef <ocool007>
2012-10-16 11:04:41 -0400

    Empty project, clean state

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+ngFragsApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ app/scripts/ng-frags.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ngFragsApp = angular.module('ngFragsApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
